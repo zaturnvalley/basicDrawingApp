a31ff52525466babd4312671b077664e954f80be
zaturnvalley <zacharycmason@gmail.com>
2017-01-05 13:45:41 -0800

    canvas is same height and width and window

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
 (function(){
   var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
 })()
