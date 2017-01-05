d5e12857302304d59b26e0ffa35e56cc619efa74
zaturnvalley <zacharycmason@gmail.com>
2017-01-05 14:14:22 -0800

    putpoint conditional checks for dragging

--- code.js
+++ code.js
@@ -10,16 +10,18 @@
   canvas.height = window.innerHeight;
 
   function putPoint(e) {
-    context.beginPath();
+    if(dragging){
+      context.beginPath();
 
-    // These are the arc method's parameters:
-    // context.arc(x, y, radius, start, end, rotate (true or false));
+      // These are the arc method's parameters:
+      // context.arc(x, y, radius, start, end, rotate (true or false));
 
-    // This will build a full circle using starting at 0 and going to 2 PI
-    context.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
+      // This will build a full circle using starting at 0 and going to 2 PI
+      context.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
 
-    // This will fill the circle fully
-    context.fill();
+      // This will fill the circle fully
+      context.fill();
+    }
   }
   // Function sets dragging to true
   function engage(){
