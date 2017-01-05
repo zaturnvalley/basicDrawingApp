ba6e01c46febb344238f17c3ca16c1044edbd835
zaturnvalley <zacharycmason@gmail.com>
2017-01-05 15:33:31 -0800

    radius js file, pulls in radius vars

---
+++ radius.js
@@ -0,0 +1,8 @@
+(function(){
+  var minRadius = 0.5,
+      maxRad = 100,
+      defaultRad = 20,
+      radSpan = document.getElementById('radVal'),
+      decRad = document.getElementById('decRad'),
+      incRad = document.getElementById('incRad');
+})();
