7c51ff8562059b10695cf6675d7e20a9d71debfc
zaturnvalley <zacharycmason@gmail.com>
2017-01-06 10:39:42 -0800

    grab swatch class, loop over array & add click event

---
+++ js/colors.js
@@ -0,0 +1,7 @@
+// Grab array of all elems with class swatch
+var swatches = document.getElementsbyClassName('swatch');
+
+// Loop over array, cache value of n
+for (var i = 0; n = swatches.length; i < n; i++) {
+  swatches[i].addEventListener('click', setSwatch)
+}
