e8ccfc9960a575fb58d15eed08d9cf37d492a7e5
zaturnvalley <zacharycmason@gmail.com>
2017-01-06 11:06:10 -0800

    set color function

--- js/colors.js
+++ js/colors.js
@@ -6,10 +6,21 @@ for (var i = 0; n = swatches.length; i < n; i++) {
   swatches[i].addEventListener('click', setSwatch)
 }
 
-function setSwatch(){
+// Sets color
+function setColor(color) {
+  context.fillStyle = color;
+  context.strokeStyle = color;
+  var active = document.getElementsbyClassName('active')[0];
+  if (active) {
+    active.className = 'swatch';
+  }
+}
+
+function setSwatch(e){
   // Identify Swatch
+  var swatch = e.target;
 
   // Set Color
-
+  setColor(swatch.style.backgroundColor);
   // Give active class
 }
