fd882e6f2e526acf38d19f0398db62677c270122
zaturnvalley <zacharycmason@gmail.com>
2017-01-07 21:04:11 -0800

    set up xhr for save func

--- js/save.js
+++ js/save.js
@@ -7,7 +7,22 @@ saveButton.addEventListener('click', saveImage);
 function saveImage() {
   var data = canvas.toDataURL();
 
-  // takes 3 parameters: window.open(url, name, options);
-  window.open(data, '_blank', 'location=0, menubar=0');
-  console.log('done')
+  // Takes 3 parameters: window.open(url, name, options);
+  // Would be done like so, but will use ajax and php
+  // window.open(data, '_blank', 'location=0, menubar=0');
+
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if(request.readyState == 4 && request.status == 200) {
+      // do response
+      var response = request.responseText;
+      console.log(response);
+    }
+  }
+
+  // true is a boolean for synchronus 
+  request.open('POST', 'save.php', true);
+  request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+  request.send('img='+data);
 }
