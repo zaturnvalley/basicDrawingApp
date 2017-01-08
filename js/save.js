f6f9ee97624e13c5cce9fab816e10a7fd3402de3
zaturnvalley <zacharycmason@gmail.com>
2017-01-07 20:56:12 -0800

    united tested fixed err

--- js/save.js
+++ js/save.js
@@ -5,8 +5,9 @@ var saveButton = document.getElementById('save');
 saveButton.addEventListener('click', saveImage);
 
 function saveImage() {
-  var data = canvas.toDataUrl();
+  var data = canvas.toDataURL();
 
   // takes 3 parameters: window.open(url, name, options);
   window.open(data, '_blank', 'location=0, menubar=0');
+  console.log('done')
 }
