b386bddde4413bc3bde24255e1a461063e3d7ee7
zaturnvalley <zacharycmason@gmail.com>
2017-01-07 20:54:20 -0800

    added parameters for saving file

--- js/save.js
+++ js/save.js
@@ -6,4 +6,7 @@ saveButton.addEventListener('click', saveImage);
 
 function saveImage() {
   var data = canvas.toDataUrl();
+
+  // takes 3 parameters: window.open(url, name, options);
+  window.open(data, '_blank', 'location=0, menubar=0');
 }
