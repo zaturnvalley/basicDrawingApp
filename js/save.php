4c8ff8de426a77287c146a0af8e8f53b6046edad
zaturnvalley <zacharycmason@gmail.com>
2017-01-07 21:09:01 -0800

    added img dir, pulled into php file

--- js/save.php
+++ js/save.php
@@ -3,7 +3,10 @@
 $data = $_POST['img'];
 
 $data = str_replace('data:image/png;base64,', '', $data);
+$data = str_replace(' ', '+', $data);
 
-$img = base64_decode($data)
+$img = base64_decode($data);
+
+$path = 'img/' . uniqid() .
 
 ?>
